a93908b37ee3b269a7b9c8764d860b2700bedae7
jeremie.robles@mainbot.me <jeremierobles@gmx.fr>
2021-03-25 13:25:47 +0100

    firt push, we ave GGA time, and GLL trame ready, we need getter func

---
+++ neo-m_gps_lib.cpp
@@ -0,0 +1,140 @@
+#include "neo-m_gps_lib.h"
+
+void gps_init()
+{
+    // Start the software serial port at the GPS's default baud
+    gpsSerial.begin(GPSBaud);
+}
+
+void gps_loop()
+{
+  static uint8_t  buffer[82];
+  static uint8_t  i  = 0;
+    // Displays information when new sentence is available.
+  while (gpsSerial.available() > 0)
+  {
+    uint8_t tmp = gpsSerial.read();
+    //Serial.write(tmp);
+
+    if (tmp != '\n')
+    {
+      buffer[i] = tmp;
+      i++;  
+    }
+    else
+    {
+      // end trame
+      uint8_t type = buffer[3] + buffer[4] + buffer[5];
+
+
+      switch (type)
+      {
+        case 207:   // GGA
+          GP_GGA_t.update = 1;
+
+            // *************  start get time *************
+          {
+
+            uint8_t cursor    = get_cursor(buffer, GGA_START_TIME);
+
+            uint8_t high_bit  = cursor;
+            uint8_t low_bit   = cursor + 1;
+
+            for (uint8_t i = 0; i < 3; i++)
+            {
+              GP_GGA_t.time[i] = ((buffer[high_bit]-48) * 10) + (buffer[low_bit]-48);
+
+              high_bit  += 2;
+              low_bit   += 2;
+            }
+
+            //Serial.println(GP_GGA_t.time[2]);
+          }
+          
+          
+            // *************  end get time *************
+
+          break;
+        
+        case 223:   // GLL
+          uint8_t cursor = get_cursor(buffer, GLL_AVAILIDABLE_DATA);
+
+          if(buffer[cursor] == 'A')
+          {
+            GP_GLL_t.update = 1;
+
+            // get degreeses data, min max -90 to 90
+            cursor = get_cursor(buffer, GLL_START_LATITUDE);
+            GP_GLL_t.latitude_deg = ((buffer[cursor]-48) * 10) + (buffer[cursor + 1]-48);
+
+            // example trame $GPGLL,4458.44931,N,00152.73190,E,114654.00,A,A*66
+            // get minute data
+            GP_GLL_t.latitude_min = ((buffer[cursor + 2]-48) * 10) + (buffer[cursor + 3]-48);
+            GP_GLL_t.latitude_min += (((buffer[cursor + 5]-48) * 10) + (buffer[cursor + 6]-48)) * 0.01;
+            
+            // get direction (N,S,O,W) data
+            cursor = get_cursor(buffer, GLL_START_LATITUDE_DIRECTION);
+            GP_GLL_t.latitude_direction = buffer[cursor];
+            
+
+
+
+            // get degreeses data, min max -180 to 180
+            cursor = get_cursor(buffer, GLL_START_LONGITUDE);
+            GP_GLL_t.longitude_deg = (((buffer[cursor]-48) * 100) + ((buffer[cursor + 1]-48) * 10) +
+                                       (buffer[cursor + 2]-48));
+
+            // example trame $GPGLL,4458.44931,N,00152.73190,E,114654.00,A,A*66
+            // get minute data
+            GP_GLL_t.longitude_min = ((buffer[cursor + 3]-48) * 10) + (buffer[cursor + 4]-48);
+            GP_GLL_t.longitude_min += (((buffer[cursor + 6]-48) * 10) + (buffer[cursor + 7]-48)) * 0.01;
+            
+            // get direction (N,S,O,W) data
+            cursor = get_cursor(buffer, GLL_START_LONGITUDE_DIRECTION);
+            GP_GLL_t.longitude_direction = buffer[cursor];
+           
+            //Serial.write( GP_GLL_t.longitude_direction);
+          }
+
+          //Serial.write( buffer[GLL_AVAILIDABLE_DATA]);
+          
+          break;
+
+        case 219:   // GSA
+          GP_GSA_t.update = 1;
+          break;
+
+        case 240:   // GSV
+          GP_GSV_t.update = 1;
+          break;
+
+        case 241:   // VTG
+          GP_VTG_t.update = 1;
+          break;
+
+        case 226:   // RMC
+          GP_RMC_t.update = 1;
+          break;
+
+        default:
+          break;
+      }
+
+      for (i; i > 0; i--)  // clear buffer
+        buffer[i] = 0;
+    }
+    
+    //Serial.write(temp[0]);
+  }
+}
+
+uint8_t get_cursor(uint8_t *ptr, uint8_t count)
+{
+  uint8_t cursor = 0;
+
+  for (uint8_t i = 0; i <= count; cursor++)
+    if (ptr[cursor] == ',')
+      i++;
+
+  return (cursor);
+}
---
+++ neo-m_gps_lib.h
@@ -0,0 +1,96 @@
+#ifndef NEO6M_LIB_H_
+#define NEO6M_LIB_H_
+
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+
+// Choose two Arduino pins to use for software serial
+#define RXPin  2
+#define TXPin  3
+
+//Default baud of NEO-6M is 9600
+#define GPSBaud 9600
+
+
+#define GGA_START_TIME 0
+//  add other GGA define..
+
+#define GLL_START_LATITUDE              0
+#define GLL_START_LATITUDE_DIRECTION    1
+#define GLL_START_LONGITUDE             2
+#define GLL_START_LONGITUDE_DIRECTION   3
+#define GLL_AVAILIDABLE_DATA            5
+//  add other GLL define..
+
+
+
+/*
+'$' suivi par un groupe de 2 lettres pour l'identifiant du récepteur. (Non limitatif):
+    •GP pour Global Positioning System.
+    •LC Loran-C receiver.
+    •OM Omega Navigation receiver.
+    •II Integrated Instrumentation (eg. AutoHelm Seatalk system).    
+
+Puis un groupe de 3 lettres pour l'identifiant de la trame.
+    •GGA : pour GPS Fix et Date.
+    •GLL : pour Positionnement Géographique Longitude - Latitude.
+    •GSA : pour DOP et satellites actifs.
+    •GSV : pour Satellites visibles.
+    •VTG : pour Direction (cap) et vitesse de déplacement (en noeuds et Km/h).
+    •RMC: pour données minimales exploitables spécifiques.
+*/
+static struct GP_GGA
+{
+    uint8_t     update  = 0;
+    uint8_t     time[3] = {0};    // H (1 bytes)| M (1 bytes)| S (1 bytes)|
+    /* data */
+}GP_GGA_t;
+
+static struct GP_GLL
+{
+    uint8_t update = 0;
+
+    int8_t  latitude_deg = 0;
+    float   latitude_min = 0;
+    uint8_t latitude_direction = 0;
+
+    int8_t  longitude_deg = 0;
+    float   longitude_min = 0;
+    uint8_t longitude_direction = 0;
+
+    /* data */
+}GP_GLL_t;
+
+static struct GP_GSA
+{
+    uint8_t update = 0;
+    /* data */
+}GP_GSA_t;
+
+static struct GP_GSV
+{
+    uint8_t update = 0;
+    /* data */
+}GP_GSV_t;
+
+static struct GP_VTG
+{
+    uint8_t update = 0;
+    /* data */
+}GP_VTG_t;
+
+static struct GP_RMC
+{
+    uint8_t update = 0;
+    /* data */
+}GP_RMC_t;
+
+// Create a software serial port called "gpsSerial"
+static SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void gps_init();
+void gps_loop();
+
+uint8_t get_cursor(uint8_t *ptr, uint8_t count);
+
+#endif
